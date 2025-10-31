cd113890b4c5830e429a17fc071176e119c1c838
dimeken <dnurbek@nic.ru>
2025-10-31 16:52:22 +0500

    added function save_to_json

--- video_stats.py
+++ video_stats.py
@@ -1,6 +1,9 @@
 import requests as re
 import os
+import json
+
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path='./.env')
 
@@ -100,6 +103,11 @@ def extract_video_data(video_ids):
         raise e
 
 
+def save_to_json(extracted_data):
+    file_path = f'./data/youtube_data_{date.today()}.json'
+
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(extracted_data, f, indent=4, ensure_ascii=False)
 
 
 
@@ -110,6 +118,7 @@ if __name__ == '__main__':
     video_ids = get_video_ids(channel_playlist_id)
     print(f'video_ids: {video_ids}')
     extracted_data = extract_video_data(video_ids)
+    save_to_json(extracted_data)
 
     print(extracted_data)
 
